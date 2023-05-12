c4a1497abb925c979b60a81f39b212d84575916b
Rupam Banerjee <rupam.banerje@gmail.com>
2023-05-12 14:31:02 +0530

    adding File Modulo

--- Divd.js
+++ Divd.js
@@ -1,1 +1,6 @@
-// We here will create a program of Dividing a number
+// We here will create a program of Dividing a numberlet dif = (a,b)=>{
+    let divd = (a,b)=>{
+        didd = a/b;
+        return console.log(didd);
+     }
+     divd(12, 34);
